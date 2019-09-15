281e6f2d3d23110bb3da984057d388510e5ee899
Serven Maraghi <maraghiserven@gmail.com>
2019-09-15 15:00:18 -0400

    Set up context API for state management

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
+import GlobalState from "./context/GlobalState";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <GlobalState>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GlobalState>,
   document.getElementById("root")
 );
 
