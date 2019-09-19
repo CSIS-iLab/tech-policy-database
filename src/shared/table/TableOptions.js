c09c53c6c317bce68b265142230ca115d47d07c6
Serven Maraghi <maraghiserven@gmail.com>
2019-09-19 16:44:25 -0400

    Create text resizing structure

--- src/shared/table/TableOptions.js
+++ src/shared/table/TableOptions.js
@@ -1,13 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import FilterBar from "./FilterBar";
+import TextResize from "./TextResize"
 
 const TableOptions = () => {
   return (
     <div>
       <SearchBar />
       <FilterBar />
-      {/* AAA Component will also go here */}
+      <TextResize />
     </div>
   );
 };
