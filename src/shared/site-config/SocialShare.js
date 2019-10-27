ea7dcab1ca78880785f529fa5ad1ec85af50e2d4
Serven Maraghi <maraghiserven@gmail.com>
2019-10-27 19:47:25 -0400

    Add social share component

--- src/shared/site-config/SocialShare.js
+++ src/shared/site-config/SocialShare.js
@@ -1,7 +1,48 @@
 import React from 'react'
+import Icon from './Icon'
 
-const Description = () => {
-  return <div className="description">Social Media</div>
+const SocialShare = () => {
+  return (
+    <ul className="share">
+      <li>
+        <a
+          href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Icon onClick={null} icon={'facebook'} />
+        </a>
+      </li>
+      <li>
+        <a
+          href={`https://twitter.com/intent/tweet?url=${window.location.href}&amp;via=CSIS&amp;related=CSIS`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Icon onClick={null} icon={'twitter'} />
+        </a>
+      </li>
+      <li>
+        <a
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}&source=CSIS`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Icon onClick={null} icon={'linkedin'} />
+        </a>
+      </li>
+      <li>
+        <a href={`mailto:techpolicy@csis.org?subject=${document.title}`}>
+          <Icon onClick={null} icon={'email'} />
+        </a>
+      </li>
+      <li>
+        <div onClick={() => window.print()}>
+          <Icon onClick={null} icon={'print'} />
+        </div>
+      </li>
+    </ul>
+  )
 }
 
-export default Description
+export default SocialShare
