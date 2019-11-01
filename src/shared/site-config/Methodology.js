a2f2d35ccafab98fbc94947915aa0a9129fe3ad5
Lindsay Urchyk <urchykli@gmail.com>
2019-11-01 12:48:13 -0400

    Grid setup

--- src/shared/site-config/Methodology.js
+++ src/shared/site-config/Methodology.js
@@ -9,8 +9,8 @@ const Methodology = () => {
   return (
     <section id="methodology" className="methodology container--inner">
       <h2 className="methodology__title">Methodology</h2>
-      <p className="methodology__desc">{methodology}</p>
-      <p className="methodology__program-desc">{program_description}</p>
+      <p className="methodology__desc">{methodology} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p className="methodology__program-desc">{program_description} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
   )
 }
