20238a5279eded81ba4bb1cf23ddcdfdc0916a05
Lindsay Urchyk <urchykli@gmail.com>
2019-11-04 11:48:38 -0500

    Update social share class names

--- src/shared/site-config/SocialShare.js
+++ src/shared/site-config/SocialShare.js
@@ -4,40 +4,40 @@ import Icon from './Icon'
 const SocialShare = () => {
   return (
     <ul className="share">
-      <li className="share-item">
+      <li className="share__item">
         <a
           href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
-          className="share-link"
+          className="share__link"
         >
           <Icon icon={'facebook'} />
         </a>
       </li>
-      <li className="share-item">
+      <li className="share__item">
         <a
           href={`https://twitter.com/intent/tweet?url=${window.location.href}&amp;via=CSIS&amp;related=CSIS`}
-          className="share-link"
+          className="share__link"
         >
           <Icon icon={'twitter'} />
         </a>
       </li>
-      <li className="share-item">
+      <li className="share__item">
         <a
           href={`https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}&source=CSIS`}
-          className="share-link"
+          className="share__link"
         >
           <Icon icon={'linkedin'} />
         </a>
       </li>
-      <li className="share-item">
+      <li className="share__item">
         <a
           href={`mailto:techpolicy@csis.org?subject=${document.title}`}
-          className="share-link share-link-email"
+          className="share__link share__link-email"
         >
           <Icon icon={'email'} />
         </a>
       </li>
-      <li className="share-item">
-        <button onClick={() => window.print()} className="share-link share-link-print">
+      <li className="share__item">
+        <button onClick={() => window.print()} className="share__link share__link-print">
           <Icon icon={'print'} />
         </button>
       </li>
