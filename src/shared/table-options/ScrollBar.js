7f000d9986fd8a6ec16900d0aaa4ebaa90f35f6e
Serven Maraghi <maraghiserven@gmail.com>
2019-11-07 16:35:42 -0500

    Add disabled class to scroll functions

--- src/shared/table-options/ScrollBar.js
+++ src/shared/table-options/ScrollBar.js
@@ -1,36 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Icon from '../site-config/Icon'
 
 const ScrollBar = () => {
+
   const [scrollPos, setScrollPos] = useState(0)
 
   const scrollLeft = () => {
     let table = document.getElementsByClassName('table__container')[0]
+    let btnLeft = document.getElementsByClassName('btn')[0]
 
     table.scrollTo(scrollPos - 300, 0)
 
     if (scrollPos - 300 < 0) {
       setScrollPos(0)
+      btnLeft.disabled = true
     } else {
       setScrollPos(scrollPos - 300)
+      btnLeft.disabled = false
     }
   }
 
   const scrollRight = () => {
     let table = document.getElementsByClassName('table__container')[0]
-    let maxScroll =
-      document.getElementsByTagName('table')[0].offsetWidth -
-      table.offsetWidth
+    let maxScroll = document.getElementsByTagName('table')[0].offsetWidth - table.offsetWidth
+    let btnRight = document.getElementsByClassName('btn')[1]
 
     table.scrollTo(scrollPos + 300, 0)
 
     if (scrollPos + 300 > maxScroll) {
       setScrollPos(maxScroll)
+      btnRight.setAttribute('disabled', true)
     } else {
       setScrollPos(scrollPos + 300)
+      btnRight.setAttribute('disabled', false) 
     }
   }
 
+  // useEffect(() => {
+      // combine scrolls here
+  // }, scrollPos)
+
+
+  
   return (
     <div className="table__scroll">
       <button
