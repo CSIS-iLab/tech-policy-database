e63901ee2010fa1b5e1f81d865f44513dfbdc6f0
Lindsay Urchyk <urchykli@gmail.com>
2019-11-07 15:00:04 -0500

    Add filter reset button

--- src/shared/table-options/CustomFilter.js
+++ src/shared/table-options/CustomFilter.js
@@ -2,15 +2,24 @@ import React from 'react'
 import Icon from '../site-config/Icon'
 
 const CustomFilter = (props) => {
+  const { handleResetFilters } = props
+
   const handleClick = () => {
     props.setFilterModalStatus(true)
   }
 
   return (
     <div className="table__filter">
-      <button className="filter-btn" onClick={handleClick}>
+      <button className="filter-btn btn btn--dark btn--icon" onClick={handleClick}>
         <Icon icon={'filter2'} />
         Filter
+      </button>
+      <button
+        className="btn btn--transparent-light btn--xs btn--icon modal__reset"
+        onClick={handleResetFilters}
+      >
+        <Icon icon={'reset'} />
+        Reset all filters
       </button>
     </div>
   )
