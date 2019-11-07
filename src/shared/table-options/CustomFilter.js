731e0898331d5ccb7a8f1fa28f357553b74744c7
Lindsay Urchyk <urchykli@gmail.com>
2019-11-07 16:11:58 -0500

    Filter button focus style

--- src/shared/table-options/CustomFilter.js
+++ src/shared/table-options/CustomFilter.js
@@ -2,22 +2,17 @@ import React from 'react'
 import Icon from '../site-config/Icon'
 
 const CustomFilter = (props) => {
-  const { handleResetFilters } = props
-
   const handleClick = () => {
     props.setFilterModalStatus(true)
   }
 
   return (
     <div className="table__filter">
-      <button className="filter-btn btn btn--dark btn--icon" onClick={handleClick}>
+      <button className="btn btn--dark btn--icon filter-btn" onClick={handleClick}>
         <Icon icon={'filter2'} />
         Filter
       </button>
-      <button
-        className="btn btn--transparent-light btn--xs btn--icon modal__reset"
-        onClick={handleResetFilters}
-      >
+      <button className="btn btn--transparent-dark btn--xs btn--icon table__reset">
         <Icon icon={'reset'} />
         Reset all filters
       </button>
