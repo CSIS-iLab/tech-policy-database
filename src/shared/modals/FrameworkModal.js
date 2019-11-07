afeb9a986d5ccfa5d3b92e4b7d3d4aa394d8eac1
Jacque Schrag <jnschrag@gmail.com>
2019-11-07 12:48:42 -0500

    Style framework modal content

--- src/shared/modals/FrameworkModal.js
+++ src/shared/modals/FrameworkModal.js
@@ -31,39 +31,42 @@ const FrameworkModal = () => {
     <aside className="modal">
       <ModalHeader title={name} onClose={onClose} />
       <section className="modal__content">
-        <dl>
-          <dt className="modal__subtitle">Organization</dt>
-          <dd className="modal__value">
-            {organization}
-            <a
-              className="modal__org-link"
-              href={website.url}
-              title="Visit this organization's website"
-            >
-              {website.name} &nbsp;
-              <Icon icon={'external_link'} />
-            </a>
-          </dd>
-          <dt className="modal__subtitle">Year</dt>
-          <dd className="modal__value">{year}</dd>
-          <dt className="modal__subtitle">Also Known As</dt>
-          <dd
-            className="modal__aka"
-            dangerouslySetInnerHTML={createMarkup(also_known_as)}
-          ></dd>
-          <dt className="modal__subtitle">Members</dt>
-          <dd className="modal__value">
-            {members.description}
+        <div className="modal__content-wrapper">
+          <dl>
+            <dt className="modal__subtitle">Organization</dt>
+            <dd className="modal__value">
+              {organization}
+              <br />
+              <a
+                className="modal__org-link"
+                href={website.url}
+                title="Visit this organization's website"
+              >
+                {website.name}
+                <Icon icon={'external-link'} />
+              </a>
+            </dd>
+            <dt className="modal__subtitle">Year</dt>
+            <dd className="modal__value">{year}</dd>
+            <dt className="modal__subtitle">Also Known As</dt>
+            <dd
+              className="modal__value modal__value--aka"
+              dangerouslySetInnerHTML={createMarkup(also_known_as)}
+            ></dd>
+            <dt className="modal__subtitle modal__subtitle--members">Members</dt>
+            <dd className="modal__value">
+              {members.description}
 
-            <ul className="modal__mem-list">
-              {members.list.map((member, i) => (
-                <li className="mem-list-item" key={i}>
-                  {member}
-                </li>
-              ))}
-            </ul>
-          </dd>
-        </dl>
+              <ul className="modal__mem-list">
+                {members.list.map((member, i) => (
+                  <li className="mem-list-item" key={i}>
+                    {member}
+                  </li>
+                ))}
+              </ul>
+            </dd>
+          </dl>
+        </div>
       </section>
       <ModalFooter link={url} />
     </aside>
