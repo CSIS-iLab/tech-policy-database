96593a45da6424a9b6dd36a7e3b07a68c4070f3a
Lindsay Urchyk <urchykli@gmail.com>
2019-11-08 12:09:37 -0500

    Class names for searchfield

--- src/shared/table-options/SearchFilter.js
+++ src/shared/table-options/SearchFilter.js
@@ -21,25 +21,31 @@ const SearchFilter = (props) => {
   }
 
   return (
-    <div className="table__search">
-      <Icon icon={'search'} />
+    <div className="table__search search search__table">
+      <Icon icon={'search'} className="search__icon search__icon-search search__icon-search-table" />
       <input
-        className="search-input"
+        className="search-input search__input-table"
         type="text"
         onChange={handleSearchText}
         placeholder="Search"
       />
-      <select className="search-dropdown" onChange={handleFilterSubject}>
+      <button className="search__button search__button-close search__button-close-table">
+        <Icon icon={'close-circle'} className="search__icon search__icon-close search__icon-close-table" />
+      </button>
+      <select className="search-dropdown search__dropdown-table" onChange={handleFilterSubject}>
         <option className="dropdown-item" value="categories">
           Categories
         </option>
-        <option className="dropdown-item" value="abbreviated_lang">
+        <option className="dropdown-item search__dropdown-item-table" value="abbreviated_lang">
           Abbreviated Language
         </option>
-        <option className="dropdown-item" value="original_lang">
+        <option className="dropdown-item search__dropdown-item-table" value="original_lang">
           Original Language
         </option>
       </select>
+      <button className="search__button search__button-triangle search__button-triangle-table">
+        <Icon icon={'arrow-triangle-down'} className="search__icon search__icon-triangle search__icon-triangle-table" />
+      </button>
     </div>
   )
 }
