21c3000e9537f02aad9c7c02c5bc1d36d2b872d3
Serven Maraghi <maraghiserven@gmail.com>
2019-11-12 17:07:09 -0500

    Update function used for changing font resize variables

--- src/shared/table-options/FontResize.js
+++ src/shared/table-options/FontResize.js
@@ -2,32 +2,32 @@ import React from 'react'
 import Icon from '../site-config/Icon'
 
 const FontResize = () => {
-  const resize = (size) => {
+  const resize = () => {
     let table = document.getElementById('table')
-    table.style.fontSize = size
+    table.style.getPropertyValue("--table-font-size");
+    table.style.setProperty("--table-font-size", '14px') 
   }
 
-  // --table-font-size: var(--font-size--md)
   return (
     <div className="font__resize">
       <button
         className="btn btn--dark btn--square"
         alt="font size small"
-        onClick={() => resize('var(--font-size-1)')}
+        onClick={() => resize('var(--font-size--sm)')}
       >
         <Icon icon={'font-size'} />
       </button>
       <button
         className="btn btn--dark btn--square"
         alt="font size medium"
-        onClick={() => resize('var(--font-size-1)')}
+        onClick={() => resize('var(--font-size--md)')}
       >
         <Icon icon={'font-size'} />
       </button>
       <button
         className="btn btn--dark btn--square"
         alt="font size large"
-        onClick={() => resize('var(--font-size-3)')}
+        onClick={() => resize('var(--font-size--lg)')}
       >
         <Icon icon={'font-size'} />
       </button>
