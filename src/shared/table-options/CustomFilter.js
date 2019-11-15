5d8c4f166cbeedaa6cd8c34552de9c44fa594a2e
Serven Maraghi <maraghiserven@gmail.com>
2019-11-14 19:58:54 -0500

    Apply active class to filter button

--- src/shared/table-options/CustomFilter.js
+++ src/shared/table-options/CustomFilter.js
@@ -3,13 +3,13 @@ import Icon from '../site-config/Icon'
 
 const CustomFilter = (props) => {
   const {
-    setFilterModalStatus, 
-    setFilteredRows, 
-    setFilteredHeaders, 
-    isFiltered, 
-    setIsFiltered, 
-    allRows, 
-    allHeaders 
+    setFilterModalStatus,
+    setFilteredRows,
+    setFilteredHeaders,
+    isFiltered,
+    setIsFiltered,
+    allRows,
+    allHeaders
   } = props
 
   const handleClick = () => {
@@ -22,18 +22,27 @@ const CustomFilter = (props) => {
     setIsFiltered(false)
   }
 
+  const renderClass = () => {
+    return isFiltered
+      ? 'btn btn--dark btn--icon filter__button js-isActive'
+      : 'btn btn--dark btn--icon filter__button'
+  }
+
   return (
     <div className="table__filter">
-      <button className="btn btn--dark btn--icon filter__button" onClick={handleClick}>
+      <button className={renderClass()} onClick={handleClick}>
         <Icon icon={'filter2'} />
         Filter
       </button>
-      {isFiltered ?
-      <button className="btn btn--xs btn--icon btn--transparent-dark filter__reset" onClick={handleResetFilters}>
-        <Icon icon={'reset'} />
-        Reset all filters
-      </button>
-      : null }
+      {isFiltered ? (
+        <button
+          className="btn btn--xs btn--icon btn--transparent-dark filter__reset"
+          onClick={handleResetFilters}
+        >
+          <Icon icon={'reset'} />
+          Reset all filters
+        </button>
+      ) : null}
     </div>
   )
 }
