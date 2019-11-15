a14863c494b52643b8cfd618bc35f6951b35ba3b
Serven Maraghi <maraghiserven@gmail.com>
2019-11-15 13:49:55 -0500

    Add modal overlay to modal container

--- src/shared/modals/ModalContainer.js
+++ src/shared/modals/ModalContainer.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import LangModal from './LangModal'
-import FilterModal from './custom-filter/FilterModal'
-import FrameworkModal from './FrameworkModal'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalContext'
+import LangModal from './LangModal'
+import FilterModal from './custom-filter/FilterModal'
+import FrameworkModal from './FrameworkModal'
 
-const ModalContainer = () => {
-  return (
-    <React.Fragment>
-      <LangModal />
-      <FilterModal />
-      <FrameworkModal />
-    </React.Fragment>
-  )
+const ModalContainer = () => {
+  const {frameModalStatus, langModalStatus, filterModalStatus} = useContext(GlobalContext)
+
+  return (
+    frameModalStatus || langModalStatus || filterModalStatus ? 
+      <div className="modal__overlay">
+        <React.Fragment>
+          <LangModal />
+          <FilterModal />
+          <FrameworkModal />
+        </React.Fragment>
+      </div> 
+    : null 
+  )
 }
 
-export default ModalContainer
+export default ModalContainer
+
