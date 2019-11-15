0506a5c2bf7269b2b0b9a85ca3f7605362b13814
Serven Maraghi <maraghiserven@gmail.com>
2019-11-15 10:13:51 -0500

    Add active class to font resize components

--- src/shared/table-options/FontResize.js
+++ src/shared/table-options/FontResize.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Icon from '../site-config/Icon'
 
 const FontResize = () => {
+  const [activeSize, setActiveSize] = useState('sm')
+
   const resize = (size) => {
+    setActiveSize(size)
     let table = document.getElementById('table')
     table.style.setProperty('--table-font-size', 'var(--font-size--' + size + ')')
+  }
+
+  const renderClass = (size) => {
+    return activeSize === size
+      ? "btn btn--dark btn--square js-isActive"
+      : "btn btn--dark btn--square"
   }
 
   return (
     <div className="font__resize">
       <button
-        className="btn btn--dark btn--square"
+        className={renderClass('sm')}
         alt="font size small"
         onClick={() => resize('sm')}
       >
         <Icon icon={'font-size'} />
       </button>
       <button
-        className="btn btn--dark btn--square"
+        className={renderClass('md')}
         alt="font size medium"
         onClick={() => resize('md')}
       >
         <Icon icon={'font-size'} />
       </button>
       <button
-        className="btn btn--dark btn--square"
+        className={renderClass('lg')}
         alt="font size large"
         onClick={() => resize('lg')}
       >
