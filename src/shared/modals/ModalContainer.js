6c069fad50990fe3b8e72f187ee8cc1ccc6630c2
Serven Maraghi <maraghiserven@gmail.com>
2019-11-25 19:54:22 -0500

    Toggle body class to enable scroll lock when modal is active

--- src/shared/modals/ModalContainer.js
+++ src/shared/modals/ModalContainer.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import LangModal from './LangModal'
-import FilterModal from './custom-filter/FilterModal'
-import FrameworkModal from './FrameworkModal'
+import React, { useContext, useEffect } from 'react'
+import { GlobalContext } from '../../context/GlobalContext'
+import LangModal from './LangModal'
+import FilterModal from './custom-filter/FilterModal'
+import FrameworkModal from './FrameworkModal'
 
-const ModalContainer = () => {
+const ModalContainer = () => {
+  const { filterModalStatus, langModalStatus, frameModalStatus } = useContext(
+    GlobalContext
+  )
 
-  return (
-    <React.Fragment>
-      <LangModal />
-      <FilterModal />
-      <FrameworkModal />
-    </React.Fragment>
-  )
+  // toggles disabled class to prevent body from scrolling when modals are open
+  useEffect(() => {
+    let body = document.getElementsByTagName('body')[0]
+    if (filterModalStatus || langModalStatus || frameModalStatus) {
+      body.classList.add('body--disabled')
+    } else {
+      body.classList.remove('body--disabled')
+    }
+  }, [filterModalStatus, langModalStatus, frameModalStatus])
+
+  return (
+    <React.Fragment>
+      <LangModal />
+          
+      <FilterModal />
+          
+      <FrameworkModal />
+        {' '}
+    </React.Fragment>
+  )
 }
 
-export default ModalContainer
-
+export default ModalContainer
